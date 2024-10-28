ef95375798c646966819071327afd1f343addb38
Arootsy <aronrootsaert07@gmail.com>
2024-10-28 17:08:44 +0100

    feat(fxmanifest): shared scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,3 +10,4 @@ version "1.0.0"
 client_scripts { "src/client/*.lua" }
 server_scripts { "src/server/*.lua", "@oxmysql/lib/MySQL.lua" }
 shared_scripts { "@es_extended/imports.lua", "@ox_lib/init.lua" }
+file { "shared/*.lua", "locales/*.json" }	
