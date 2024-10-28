3864455271830a9a0d8929ed35dd137fbde99a7b
Arootsy <aronrootsaert07@gmail.com>
2024-10-28 17:09:30 +0100

    feat(server): server.lua finished

---
+++ src/server/server.lua
@@ -0,0 +1,39 @@
+-- // [STARTUP] \\ --
+
+lib.locale()
+local Config = require('shared.config')
+
+-- // [CALLBACKS] \\ --
+
+lib.callback.register('lm-staffduty:cb:getPlayerSkin', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    local skin = MySQL.prepare.await('SELECT skin FROM users WHERE identifier = ?', {
+        xPlayer.identifier
+    })
+
+    return json.decode(skin)
+end)
+
+-- // [COMMANDS] \\ --
+
+lib.addCommand(Config.Command, {
+    help = locale("command_help"),
+    restricted = function()
+        local restricted = {}
+
+        for group, allowed in pairs(Config.AllowedGroups) do
+            if allowed then
+                table.insert(restricted, 'group.' .. group)
+            end
+        end
+
+        return restricted
+    end,
+}, function(source, args, raw)
+    Player(source).state.isOnDuty = not Player(source).state.isOnDuty
+
+    if Player(source).state.isOnDuty then
+        TriggerClientEvent('illenium-appearance:client:loadJobOutfit', source, { outfitData = Config.Outfits[GetEntityModel(GetPlayerPed(source))] })
+    end
+end)
