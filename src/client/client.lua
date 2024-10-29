000dcb0f6a9c779e4e2e6b304df6a0b1b0e71297
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-10-29 22:15:13 +0100

    Update client.lua

--- src/client/client.lua
+++ src/client/client.lua
@@ -1,30 +1,6 @@
 -- // [STARTUP] \\ --
 lib.locale()
 
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(PlayerData)
-    ESX.PlayerData = PlayerData
-    ESX.PlayerLoaded = true
-end)
-
-RegisterNetEvent('esx:onPlayerLogout')
-AddEventHandler('esx:onPlayerLogout', function()
-    ESX.PlayerLoaded = false
-end)
-
-RegisterNetEvent('esx:setJob')
-AddEventHandler('esx:setJob', function(Job)
-    ESX.PlayerData.job = Job
-end)
-
--- // [THREADS] \\ --
-
-CreateThread(function()
-    while not ESX.PlayerLoaded do Wait(0) end
-
-    TriggerEvent("txcl:setAdmin", false, false, locale("no_access"))
-end)
-
 -- // [STATEBAG] \\ --
 
 AddStateBagChangeHandler('isOnDuty', nil, function(bagName, key, value, _reserved, replicated)
